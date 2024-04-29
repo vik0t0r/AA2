cb0759296d6305aaa411b841d2d23cb9505c48b3
Víctor de Pedraza Ajenjo <v.depedraza@nayarsystems.com>
2024-04-29 09:59:39 +0200

    funciona lol

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define N 32
+#define N 16
 
 void init_vector(float vec[], size_t size) {
     for (size_t i = 0; i < size; i++) {
@@ -73,6 +73,41 @@ static void avx512_dgmv( size_t n, float c[n], const float M[n][n], const float 
     }
 }
 
+// dgemm
+// realiza la operación matricial C = C + A*B, donde A, B y C son tres
+// matrices de elementos de tipo double de dimensiones:
+// A de dim1 x dim2
+// B de dim2 x dim3
+// C de dim1 x dim3
+void simple_dgemm( int dim1, int dim2, int dim3, float *A, float *B, float *C) {
+    for (int i=0; i<dim1; i++){
+        for (int j=0; j<dim2; j++){
+            for (int k=0; k<dim3; k++){
+                //C[i][k] += C[i][k] + A[i][j]*B[j][k];
+                *(C+i*dim3+k) += *(A+i*dim2+j) * *(B+j*dim3+k);
+            }
+        }
+    }
+}
+
+void avx512_dgemm(int dim1, int dim2, int dim3, float *A, float *B, float *C) {
+    for (int i = 0; i < dim1; i++) {
+        for (int j = 0; j < dim2; j++) {
+            __m512 avec = _mm512_set1_ps(*(A + i * dim2 + j));
+            for (int k = 0; k < dim3; k += 16) {
+
+                __m512 bvec = _mm512_loadu_ps(B + j * dim3 + k);
+
+                __m512 cvec = _mm512_loadu_ps(C + i * dim3 + k);
+
+                cvec = _mm512_fmadd_ps(avec, bvec, cvec);
+
+                _mm512_storeu_ps(C + i * dim3 + k, cvec);
+            }
+        }
+    }
+}
+
 
 int main() {
     srand(2384097);
@@ -108,4 +143,48 @@ int main() {
     avx512_dgmv(N, res3,M,b);
     printf("\nVector c (avx512_dgmv):\n");
     print_vector(res3, N);
+
+    printf("------------------------------------------------------------------\n\n\n");
+    // dgemm
+// realiza la operación matricial C = C + A*B, donde A, B y C son tres
+// matrices de elementos de tipo double de dimensiones:
+// A de dim1 x dim2
+// B de dim2 x dim3
+// C de dim1 x dim3
+
+    int dim1,dim2,dim3;
+    dim1 = N;
+    dim2 = N;
+    dim3 = N;
+
+    float A[dim1][dim2];
+    float B[dim2][dim3];
+    float C[dim1][dim3];
+
+    float res1_1[dim1][dim3];
+    float res2_2[dim1][dim3];
+
+
+    init_matrix(A, N);
+    printf("\nMatriz A:\n");//as
+    print_matrix(A, N);
+
+    init_matrix(B, N);
+    printf("\nMatriz B:\n");//as
+    print_matrix(B, N);
+
+    init_matrix(C, N);
+    printf("\nMatriz C:\n");//as
+    print_matrix(C, N);
+    memcpy(res1_1, C, sizeof(float)*dim1*dim3);
+    memcpy(res2_2, C, sizeof(float)*dim1*dim3);
+
+
+    simple_dgemm(dim1,dim2,dim3, A,B,res1_1);
+    printf("\nMatriz C (simple_dgemm):\n");
+    print_matrix(res1_1, N);
+
+    avx512_dgemm(dim1,dim2,dim3, A,B,res2_2);
+    printf("\nMatriz C (avx512_dgemm):\n");
+    print_matrix(res2_2, N);
 }
