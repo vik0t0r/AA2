bcfa276ac7e4643eeb4f5fb8a6734c478c29e9d6
Víctor de Pedraza Ajenjo <v.depedraza@nayarsystems.com>
2024-04-29 08:50:34 +0200

    boilerplat, not intrinsics

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #include <immintrin.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define N 8
 
 void init_vector(float vec[], size_t size) {
     for (size_t i = 0; i < size; i++) {
-        vec[i] = (float)rand() / RAND_MAX;
+        vec[i] = (float)(rand() % 10);
     }
 }
 
 void init_matrix(float mat[][N], size_t size) {
     for (size_t i = 0; i < size; i++) {
         for (size_t j = 0; j < size; j++) {
-            mat[i][j] = (float)rand() / RAND_MAX;
+            mat[i][j] = (float)(rand() % 10);
         }
     }
 }
@@ -46,30 +47,58 @@ static void simple_dgmv( size_t n , float c[ n ], const float M[n][n], const flo
 }
 
 // c = c + M * b
+static void unroll_dgmv( size_t n, float c[n], const float M[n][n], const float b[n]) {
+    for (int i = 0 ; i<n ; i++)
+        for (int j = 0 ; j<n ; j += 4)
+            c[i] += M[i][j+0]*b[j+0]+
+                    M[i][j+1]*b[j+1]+
+                    M[i][j+2]*b[j+2]+
+                    M[i][j+3]*b[j+3];
+}
+
+// c = c + M * b
+static void avx512_dgmv( size_t n, float c[n], const float M[n][n], const float b[n]) {
+    for (int i = 0 ; i<n ; i++)
+        for (int j = 0 ; j<n ; j += 4)
+            c[i] += M[i][j+0]*b[j+0]+
+                    M[i][j+1]*b[j+1]+
+                    M[i][j+2]*b[j+2]+
+                    M[i][j+3]*b[j+3];
+}
 
 
 int main() {
     srand(2384097);
 
-    float b[N], c[N];
+    float b[N], c[N], res1[N], res2[N],res3[N];
     float M[N][N];
 
-    init_vector(b, N);
     init_vector(c, N);
+    init_vector(b, N);
     init_matrix(M, N);
-
-    printf("Vector b:\n");
-    print_vector(b, N);
 
     printf("\nVector c:\n");
     print_vector(c, N);
 
+    printf("Vector b:\n");
+    print_vector(b, N);
+
     printf("\nMatriz M:\n");//as
     print_matrix(M, N);
+    memcpy(res1, c, sizeof(float)*N);
+    memcpy(res2, c, sizeof(float)*N);
+    memcpy(res3, c, sizeof(float)*N);
 
-    simple_dgmv(N, c,M,b);
 
-    printf("\nVector c (resultado):\n");
-    print_vector(c, N);
+    simple_dgmv(N, res1,M,b);
+    printf("\nVector c (simple_dgmv):\n");
+    print_vector(res1, N);
 
+    unroll_dgmv(N, res2,M,b);
+    printf("\nVector c (unroll_dgmv):\n");
+    print_vector(res2, N);
+
+    avx512_dgmv(N, res3,M,b);
+    printf("\nVector c (avx512_dgmv):\n");
+    print_vector(res3, N);
 }
