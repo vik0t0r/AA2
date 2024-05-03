84a201b91aeceb27f1cab8be3a32b3f3b3f39acf
Víctor de Pedraza Ajenjo <v.depedraza@nayarsystems.com>
2024-05-03 13:31:09 +0200

    funciona solo con matrices cuadradas

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define N 7
+#define N 18
 
 void init_vector(float vec[], size_t size) {
     for (size_t i = 0; i < size; i++) {
@@ -19,10 +19,10 @@ void init_matrix(float mat[][N], size_t size) {
     }
 }
 
-void init_matrix_rc(float **mat, size_t rows, size_t cols) {
+void init_matrix_rc(float *mat, size_t rows, size_t cols) {
     for (size_t i = 0; i < rows; i++) {
         for (size_t j = 0; j < cols; j++) {
-            mat[i][j] = (float)(rand() % 10);
+            mat[i*rows+j] = (float)(rand() % 10);
         }
     }
 }
@@ -199,9 +199,9 @@ int main() {
 // C de dim1 x dim3
 
     int dim1,dim2,dim3;
-    dim1 = N;
-    dim2 = N;
-    dim3 = N;
+    dim1 = 3;
+    dim2 = 3;
+    dim3 = 3;
 
     float A[dim1][dim2];
     float B[dim2][dim3];
@@ -211,15 +211,15 @@ int main() {
     float res2_2[dim1][dim3];
 
 
-    init_matrix(A, N);
+    init_matrix_rc(A, dim1,dim2);
     printf("\nMatriz A:\n");//as
     print_matrix(A, N);
 
-    init_matrix(B, N);
+    init_matrix_rc(B, dim2,dim3);
     printf("\nMatriz B:\n");//as
     print_matrix(B, N);
 
-    init_matrix(C, N);
+    init_matrix_rc(C, dim1,dim3);
     printf("\nMatriz C:\n");//as
     print_matrix(C, N);
     memcpy(res1_1, C, sizeof(float)*dim1*dim3);
@@ -233,4 +233,10 @@ int main() {
     avx512_dgemm(dim1,dim2,dim3, A,B,res2_2);
     printf("\nMatriz C (avx512_dgemm):\n");
     print_matrix(res2_2, N);
+
+    if (memcmp(res1_1, res2_2,dim1*dim3*sizeof(float)) == 0){
+        printf("\n--------\n\nMATRIXES EQUAL\n\n");
+    } else {
+        printf("\n--------\n\n ERROR\n\n");
+    }
 }
